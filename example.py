2ac91f40b86aa674f83b6f2be4ebe403b40e6f76
Robert Einhorn <robert.einhorn.hu@gmail.com>
2024-02-10 18:59:03 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,2 @@
-def headline2(
-    text,           # type: str
-    width=80,       # type: int
-    fill_char='-',  # type: str
-):                  # type: (...) -> str
-    return f" {text.title()} ".center(width, fill_char)
+while (block := f.read(256)) != '':
+    process(block)
